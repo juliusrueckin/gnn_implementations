2a8c86a372347f35a11ab50f231de30db1909291
Julius Rückin <julius.rueckin@gmail.com>
2021-12-13 15:36:31 +0100

    add GATv2 network

--- graph_attention_networks/gat_networks.py
+++ graph_attention_networks/gat_networks.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GATConv
+from torch_geometric.nn import GATConv, GATv2Conv
 
 
 class GAT(torch.nn.Module):
@@ -8,6 +8,22 @@ class GAT(torch.nn.Module):
         super().__init__()
         self.conv1 = GATConv(num_node_features, 16, heads=8, dropout=0.2, concat=True)
         self.conv2 = GATConv(16 * 8, num_classes, heads=1, concat=False, dropout=0.2)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training, p=0.2)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+
+class GATv2(torch.nn.Module):
+    def __init__(self, num_node_features: int, num_classes: int):
+        super().__init__()
+        self.conv1 = GATv2Conv(num_node_features, 16, heads=8, dropout=0.2, concat=True)
+        self.conv2 = GATv2Conv(16 * 8, num_classes, heads=1, dropout=0.2, concat=False)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
